7def7ec615dd76ba461c517258956dd859c2b12f
Rune Nielsen <runenielsen@runbox.com>
2021-03-20 22:36:55 +0100

    init project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to skattelisten-csv-parser
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/skattelisten_csv_parser/core.clj
@@ -0,0 +1,37 @@
+(ns skattelisten-csv-parser.core
+  (:require [clojure.java.io :as io])
+  (:require [clojure.data.json :as json])
+  (:require [clojure.string :as str])
+  (:gen-class))
+
+(defn create-company
+  [splitted-line]
+  {:csv (nth splitted-line 0)
+   :name (nth splitted-line 1)
+   :se (nth splitted-line 2)
+   :incomeYear (nth splitted-line 3)
+   :companyType (nth splitted-line 5)
+   :taxableIncome (nth splitted-line 8)
+   :deficit (nth splitted-line 9)
+   :corporateTax (nth splitted-line 10)})
+
+(defn convert
+  [csv-line]
+  (->> (str/split csv-line #",")
+       create-company
+       json/write-str
+       (apply str ",")))
+
+(defn -main
+  [& args]
+  (def input-path (nth args 0))
+  (def output-path (nth args 1))
+  (spit output-path "[")
+  (spit output-path
+        (subs (->> input-path
+                   io/reader
+                   line-seq
+                   (drop 1)
+                   (map convert)
+                   (apply str)) 1) :append true)
+  (spit output-path "]" :append true))
